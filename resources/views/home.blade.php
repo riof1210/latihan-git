e5bc605d7505b006fb066a919b898170a51b643b
riof1210 <riofadli097@gmail.com>
2021-08-04 14:48:58 +0700

    menambahkan file pada views dan mengubah file web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Belajar Blog | Home</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="/">Belajar Blog</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/posts">Post</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    
+      <div class="container mt-4">
+          <h1>Halaman Beranda</h1>
+          <h3>Selamat Datang Di Blog Kami!</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel fuga mollitia exercitationem impedit at minima quos saepe est sit tempora libero, consequuntur magnam laboriosam? Laudantium ullam debitis magni sit illum magnam! Officia ut laboriosam iste, nihil magni vero odit nostrum expedita fugit voluptatem magnam dolore deleniti quasi, eum exercitationem ullam adipisci perferendis quos voluptatum aut officiis! Molestiae accusamus fugit expedita repudiandae sapiente consequuntur obcaecati perspiciatis omnis. Harum nesciunt eum a cupiditate quis maiores voluptates laboriosam impedit sint accusantium voluptas facere magnam ea, animi veritatis, amet dolore nobis pariatur voluptate rerum praesentium? Dolores repellat necessitatibus, quo repudiandae saepe eos fugiat.</p>
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
